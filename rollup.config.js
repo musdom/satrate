49abb3c799f55b9b4791df64623753a5dd116ddf
Adam Mashrique <amashrique@gmail.com>
2020-01-31 00:36:35 +0800

    bulma

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -32,7 +33,7 @@ export default {
     // https://github.com/rollup/rollup-plugin-commonjs
     resolve({ browser: true }),
     commonjs(),
-
+    postcss(),
     // Watch the `public` directory and refresh the
     // browser on changes when not in production
     !production && livereload('public'),
--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
+  import 'bulma/css/bulma.css';
 	import { onMount } from "svelte";
   import moment from "moment";
 	
 	let myr = 1;
+  let myrDisp = "";
 	let sat = 1;
+  let satDisp = "";
 	let satRate = null;
   let elapsed = "";
 	
@@ -20,29 +23,101 @@
   });
 	
 	function getMYR() {
+    if (typeof sat === 'undefined') return;
 		myr = sat*satRate;
-		myr = myr.toLocaleString('en-MY', { maximumFractionDigits: 2 });
+    separatorDisplay();
 	}
 	
 	function getSAT() {
+    if (typeof myr === 'undefined') return;
 		sat = myr/satRate;
-		sat = sat.toLocaleString('en-MY', { maximumFractionDigits: 4 });
+    separatorDisplay()
 	}
+
+  function separatorDisplay() {
+    satDisp = sat.toLocaleString('en-MY', { maximumFractionDigits: 4 });
+    myrDisp = myr.toLocaleString('en-MY', { maximumFractionDigits: 2 });
+  }
 </script>
 
 <main>
-  <label>
-    MYR
-    <input type=text bind:value={myr} on:input={getSAT} on:change={getSAT} min=0>
-  </label>
+  <section class="section is-medium">
 
-  <label>
-    SAT
-    <input type=text bind:value={sat} on:input={getMYR} on:change={getMYR} min=0>
-  </label>
+    <div class="columns is-centered">
+      <div class="column is-1">
+        <label class="label has-text-white">MYR</label>
+      </div>
+      <div class="column is-4">
+        <!-- <label class="label has-text-white">MYR</label> -->
+        <input 
+          class="input is-large" 
+          type="number" 
+          bind:value={myr} 
+          on:input={getSAT} on:change={getSAT} 
+          min=0
+        />
+      </div>
 
-  <p>
-    1 MYR = { Math.floor(1/satRate) } satoshis<br/>
-    Exchange rate from Luno, { elapsed }
-  </p>
+      <div class="column is-5">
+        <!-- <label class="label has-text-white">MYR</label> -->
+        <input 
+          class="input is-large has-background-dark has-text-light" 
+          type="text" 
+          bind:value={myrDisp} 
+          readonly
+        />
+      </div>
+    </div>
+
+    <div class="columns is-centered">
+      <div class="column is-1">
+        <label class="label has-text-white">SAT</label>
+      </div>
+
+      <div class="column is-4">
+        <!-- <label class="label has-text-white">SAT</label> -->
+        <input 
+          class="input is-large" 
+          type="number" 
+          bind:value={sat} 
+          on:input={getMYR} on:change={getMYR} 
+          min=0
+        />
+      </div>
+
+      <div class="column is-5">
+        <!-- <label class="label has-text-white">SAT</label> -->
+        <input 
+          class="input is-large has-background-dark has-text-light" 
+          type="text" 
+          bind:value={satDisp} 
+          readonly
+        />
+      </div>
+
+    </div>
+
+    <div class="columns is-centered">
+      <div class="column is-10">
+        <p>Exchange rate from Luno, { elapsed }</p>
+      </div>
+    </div>
+
+  </section>
+
 </main>
+
+<style>
+  main {
+    min-height: 100vh;
+    /* align-content: center;
+    margin: 0 auto; */
+    color: white;
+  }
+  /* .columns {
+    border: 1px solid white
+  } */
+  /* html {
+    background-color: hsl(258, 10%, 19%);
+  } */
+</style>
